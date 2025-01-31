a50fffb168ff5d3ab00232578ec3a6e918b8d494
Alixo777 <alixo777@gmail.com>
2025-01-31 12:04:47 +0200

    2nd alixo

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -4,6 +4,18 @@ main().catch(err => console.log(err));
 
 async function main() {
 
-    await mongoose.connect('mongodb://host.docker.internal:27017/users');
+    await mongoose.connect('mongodb://admin:admin@mongo:27017/projects');
     console.log("mongo connect started");
 }
+// mongosh -u admin //pass: admin
+
+//use patient // name of db
+// db.createUser(
+//     {
+//       user: "admin",
+//       pwd: "admin",  // Or  "<cleartext password>"
+//       roles: [ "readWrite", "dbAdmin" ]
+//     }
+//  )
+ 
+ 
